9d6e728dcdad4d04d3d61ed0abb7acf044b4fc24
Jeonghoon <43024845+rhenus9911@users.noreply.github.com>
2023-11-27 16:57:59 +0900

    Revert "데이터 받아서 필터링 후 지도에 표시하는 것까지 구현했습니다."

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def index():
 
 @app.route('/report')
 def report():
-    gu = request.args.get('gu')
     dong = request.args.get('dong')
     if dong:
         sales_img, density_img, trans_sales, trans_den = table_info(dong)
@@ -17,11 +16,9 @@ def report():
         return redirect('/')
 
     return render_template('report.html',
-                            gu = str(gu),
                             dong = str(dong),
                             sales_dict = trans_sales,
                             den_dict = trans_den,
-                            recommend = list(trans_den.items())[0][0],
                             graph1 = sales_img,
                             graph2 = density_img)
 
